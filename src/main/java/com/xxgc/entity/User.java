4c4ed29bd933eed1f171cf65cfcfc9e4dee8db77
gaoyazb2021%40163.com <Wangxianci01>
2024-10-01 15:59:39 +0800

    项目

---
+++ src/main/java/com/xxgc/entity/User.java
@@ -0,0 +1,12 @@
+package com.xxgc.entity;
+
+//User
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class User {
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/com/xxgc/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.xxgc.mapper;
+
+import com.xxgc.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+
+    List<User> findAll();
+}
