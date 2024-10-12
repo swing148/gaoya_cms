5545e642c24955825a0af67889a52c6b42124284
gaoyazb2021%40163.com <Wangxianci01>
2024-10-12 14:21:12 +0800

    高雅提交的实训作业2。

--- src/main/java/com/xxgc/test/gaoYaTest.java
+++ src/main/java/com/xxgc/test/gaoYaTest.java
@@ -2,6 +2,6 @@ package com.xxgc.test;
 
 public class gaoYaTest {
     public static void main(String[] args) {
-        System.out.println("我是高雅~");
+        System.out.println("我是高雅2~");
     }
 }
