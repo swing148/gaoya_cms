f3a1b93933e79266c071771c516bc8b22b489d8a
hu <2033259342@qq.com>
2024-10-16 09:39:10 +0800

    胡俊峰提交的作业

--- src/main/java/com/xxgc/test/huJunFengTest.java
+++ src/main/java/com/xxgc/test/huJunFengTest.java
@@ -2,6 +2,6 @@ package com.xxgc.test;
 
 public class huJunFengTest {
     public static void main(String[] args) {
-        System.out.println("我是胡俊峰~");
+        System.out.println("我是胡俊峰1~");
     }
 }
